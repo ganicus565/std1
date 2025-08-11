c7fa8fa1fc449fb00d3ec1b2bde62085c5915e3c
chandan_roy <chandanroy565@gmail.com>
2025-08-11 18:19:34 +0530

    yes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,6 +63,66 @@ public class GameManager : MonoBehaviour
             win = 2;
         }
 
+        if (array[3] == 1 && array[4] == 1 && array[5] == 1)
+        {
+            win = 1;
+        }
+        if (array[3] == 2 && array[4] == 2 && array[5] == 2)
+        {
+            win = 2;
+        }
+
+        if (array[6] == 1 && array[7] == 1 && array[8] == 1)
+        {
+            win = 1;
+        }
+        if (array[6] == 2 && array[7] == 2 && array[8] == 2)
+        {
+            win = 2;
+        }
+
+        if (array[0] == 1 && array[3] == 1 && array[6] == 1)
+        {
+            win = 1;
+        }
+        if (array[0] == 2 && array[3] == 2 && array[6] == 2)
+        {
+            win = 2;
+        }
+
+        if (array[1] == 1 && array[4] == 1 && array[7] == 1)
+        {
+            win = 1;
+        }
+        if (array[1] == 2 && array[4] == 2 && array[7] == 2)
+        {
+            win = 2;
+        }
+        if (array[2] == 1 && array[5] == 1 && array[8] == 1)
+        {
+            win = 1;
+        }
+        if (array[2] == 2 && array[5] == 2 && array[8] == 2)
+        {
+            win = 2;
+        }
+        if (array[2] == 1 && array[4] == 1 && array[6] == 1)
+        {
+            win = 1;
+        }
+        if (array[2] == 2 && array[4] == 2 && array[6] == 2)
+        {
+            win = 2;
+        }
+        if (array[0] == 1 && array[4] == 1 && array[8] == 1)
+        {
+            win = 1;
+        }
+        if (array[0] == 2 && array[4] == 2 && array[8] == 2)
+        {
+            win = 2;
+        }
+
         Debug.Log("won " + win);
         if (win == 1 || win == 2)
             over.SetActive(true);
