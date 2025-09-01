c86d8b3ee393ce28e89ce020595b5069e670df83
chandan_roy <chandanroy565@gmail.com>
2025-09-01 18:59:10 +0530

    no message

--- Assets/MCQ/QuestionDB.cs
+++ Assets/MCQ/QuestionDB.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 public class QuestionDB : MonoBehaviour
 {
+    [SerializeField]
+    public Question[] questions;
+    public TMP_Text  questionLvl;
+
+    [SerializeField]
+    public Text[] optLvl;
+   
     public Toggle A;
     public Toggle B;
     public Toggle C;
     public Toggle D;
-
+    int currQus =0;
+    private void Start()
+    {
+        SetNewQuestion(0);
+    }
     public void OnAnswerSelected()
     {
         if(D.isOn==true)
@@ -21,4 +33,37 @@ public class QuestionDB : MonoBehaviour
 
         }
     }
+
+    public void SetNewQuestion(int qNo)
+    {
+        questionLvl.text = questions[qNo].question;
+        for (int i = 0; i < questions[qNo].opts.Length; i++)
+        {
+            optLvl[i].text = questions[qNo].opts[i];
+        }
+
+    }
+    public void NextQst()
+    {
+        if (currQus >= questions.Length)
+            return;
+        currQus++;
+        SetNewQuestion(currQus);
+    }
+    public void PvsQst()
+    {
+        if (currQus == 0)
+            return;
+        currQus--;
+        SetNewQuestion(currQus);
+    }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string question;
+    public string[] opts;
+    public int ans;
+
 }
