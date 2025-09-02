ebe55bcb4309e57a2ac494cd4fa45a56e87c3562
chandan_roy <chandanroy565@gmail.com>
2025-09-02 18:38:58 +0530

    no message

--- Assets/MCQ/QuestionDB.cs
+++ Assets/MCQ/QuestionDB.cs
@@ -12,26 +12,18 @@ public class QuestionDB : MonoBehaviour
     [SerializeField]
     public Text[] optLvl;
    
-    public Toggle A;
-    public Toggle B;
-    public Toggle C;
-    public Toggle D;
+    public Toggle[] AnsRedio;
+
     int currQus =0;
     private void Start()
     {
         SetNewQuestion(0);
     }
-    public void OnAnswerSelected()
+    public void OnAnswerSelected(int optNo)
     {
-        if(D.isOn==true)
-        {
-            Debug.Log("correct");
-        }
-        else
-        {
-            Debug.Log("Wrong");
-
-        }
+        Debug.Log("Selected  Ans " + optNo);
+        if (AnsRedio[optNo].isOn== true)
+            questions[currQus].sectAns = optNo;
     }
 
     public void SetNewQuestion(int qNo)
@@ -40,12 +32,17 @@ public class QuestionDB : MonoBehaviour
         for (int i = 0; i < questions[qNo].opts.Length; i++)
         {
             optLvl[i].text = questions[qNo].opts[i];
+            if (i == questions[qNo].sectAns)
+                AnsRedio[i].isOn = true;
+            else
+                AnsRedio[i].isOn = false;
+
         }
 
     }
     public void NextQst()
     {
-        if (currQus >= questions.Length)
+        if (currQus >= questions.Length-1)
             return;
         currQus++;
         SetNewQuestion(currQus);
@@ -65,5 +62,6 @@ public class Question
     public string question;
     public string[] opts;
     public int ans;
+    public int sectAns;
 
 }
